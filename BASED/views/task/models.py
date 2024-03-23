b00a267325f6e38e302290f9bf675b7a9bd6b2af
ValikSilchenko <valik.silchenko@gmail.com>
2024-03-23 22:59:23 +0300

    feat: add edit task methods

--- BASED/views/task/models.py
+++ BASED/views/task/models.py
@@ -3,9 +3,14 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class CreateTaskBody(BaseModel):
+class TaskBody(BaseModel):
     title: str | None
     description: str | None
     deadline: date
     responsible_user_id: int
     days_for_completion: int
+
+
+class EditTaskBody(BaseModel):
+    task_id: int
+    task_data: TaskBody
