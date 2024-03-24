6353e0ebf17fc11bafbb15c84266008f1859d684
ValikSilchenko <valik.silchenko@gmail.com>
2024-03-24 04:46:37 +0300

    feat: add get task dependent of

--- BASED/repository/task.py
+++ BASED/repository/task.py
@@ -171,9 +171,9 @@ class TaskRepository:
 
         return bool(row)
 
-    async def get_task_depends(self, id_: int) -> TaskDepends | None:
+    async def get_task_depends(self, id_: int) -> Optional[TaskDepends]:
         """
-        Показывает зависимость задачи
+        Показывает зависимости задачи
         """
         sql = """
         SELECT * from "task_depends"
@@ -186,7 +186,20 @@ class TaskRepository:
 
         return TaskDepends(**dict(row))
 
-    async def add_task_depends(self, id_: int, depends_id: int):
+    async def get_tasks_dependent_of(self, dependent_task_id: int) -> list[TaskDepends]:
+        """
+        Получение всех задач, от которых зависит данная.
+        """
+        sql = """
+                SELECT * from "task_depends"
+                WHERE "depends_task_id" = $1
+                """
+        async with self._db.acquire() as c:
+            rows = await c.fetch(sql, dependent_task_id)
+
+        return [TaskDepends(**dict(row)) for row in rows]
+
+    async def add_task_depends(self, id_: int, depends_id: int) -> None:
         """
         Добавляет зависимость задач
         """
@@ -196,8 +209,7 @@ class TaskRepository:
         ON CONFLICT (task_id, depends_task_id) DO NOTHING
         """
         async with self._db.acquire() as c:
-            await c.fetchrow(sql, id_, depends_id)
-        return
+            await c.execute(sql, id_, depends_id)
 
     async def update_task_archive_status(
         self, task_id: int, archive_status: bool
@@ -246,7 +258,7 @@ class TaskRepository:
 
         return [ShortTask(**dict(i)) for i in data]
 
-    async def get_tasks_ordered_by_status(self):
+    async def get_tasks_ordered_by_status(self) -> list[Task]:
         """
         Получает задачи отсортированные по дедлайнам и статусам.
         """
